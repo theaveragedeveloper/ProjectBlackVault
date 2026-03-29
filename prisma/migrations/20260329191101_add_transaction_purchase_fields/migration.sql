85c66d403b572c25b9ea998a76a42eeb708b8c45
Alex Sauln <saulnalex@gmail.com>
2026-03-29 15:11:07 -0400

    feat: add purchasePrice/pricePerRound/purchaseDate to AmmoTransaction schema

---
+++ prisma/migrations/20260329191101_add_transaction_purchase_fields/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "AmmoTransaction" ADD COLUMN "pricePerRound" REAL;
+ALTER TABLE "AmmoTransaction" ADD COLUMN "purchaseDate" DATETIME;
+ALTER TABLE "AmmoTransaction" ADD COLUMN "purchasePrice" REAL;
